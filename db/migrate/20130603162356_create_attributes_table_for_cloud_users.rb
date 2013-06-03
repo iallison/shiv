1d44f764ce7489a85786ba8d4275c6fd020ac82c
JD Bottorf <jd@sdsc.edu>
2013-06-03 09:36:25 -0700

    fixes table name

--- db/migrate/20130603162356_create_attributes_table_for_cloud_users.rb
+++ db/migrate/20130603162356_create_attributes_table_for_cloud_users.rb
@@ -1,15 +1,15 @@
 class CreateAttributesTableForCloudUsers < ActiveRecord::Migration
   def self.up
-    create_table :cloud_users_attributes do |t|
+    create_table :cloud_user_attributes do |t|
       t.integer :cloud_user_id, :null => false
       t.text :name, :null => false
       t.text :value, :null => false
       t.timestamps
     end
-    add_index :cloud_users_attributes, :cloud_user_id
+    add_index :cloud_user_attributes, :cloud_user_id
   end
 
   def self.down
-    drop_table :cloud_users_attributes
+    drop_table :cloud_user_attributes
   end
 end
