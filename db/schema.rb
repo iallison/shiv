1c0c3d919d53cf9c85e953ed0f0071b829d505d0
Your Name <you@example.com>
2015-10-02 23:54:58 +0000

    added columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150619212909) do
+ActiveRecord::Schema.define(:version => 20151002201809) do
 
   create_table "box_attributes", :force => true do |t|
     t.integer  "box_id",     :null => false
@@ -104,9 +104,12 @@ ActiveRecord::Schema.define(:version => 20150619212909) do
     t.string   "operating_system"
     t.string   "os_release"
     t.string   "ip"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
     t.integer  "box_id"
+    t.string   "serial"
+    t.string   "memory"
+    t.string   "cloud_environment"
   end
 
   create_table "taggings", :force => true do |t|
